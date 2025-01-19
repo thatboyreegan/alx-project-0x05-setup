4957a43dea3643d3aa9bbdb9002a009f720c5055
thatboyreegan <reaganronald491@gmail.com>
2025-01-19 16:08:15 +0300

    fix: language syntax

--- alx-project-0x05/context/CountContext.tsx
+++ alx-project-0x05/context/CountContext.tsx
@@ -1,9 +1,9 @@
 import { createContext, useContext,  useState, ReactNode } from "react"
 
 interface CountContextProps {
-  count: number
-  increment: () => void
-  decrement: () => void
+  count: number;
+  increment: () => void;
+  decrement: () => void;
 }
 
 export const CountContext = createContext<CountContextProps | undefined>(undefined)
