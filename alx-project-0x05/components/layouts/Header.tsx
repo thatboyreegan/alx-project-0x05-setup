f5447cbeb99e7d13bd84c3e4c0f32dd021cec399
thatboyreegan <reaganronald491@gmail.com>
2025-01-19 16:02:37 +0300

    add: implementing ContextAPI for state management

--- alx-project-0x05/components/layouts/Header.tsx
+++ alx-project-0x05/components/layouts/Header.tsx
@@ -1,24 +1,25 @@
 import Link from "next/link";
 import Button from "../common/Button";
+import { usePathname } from "next/navigation";
+import { useCount } from "@/context/CountContext";
 
 const Header: React.FC = () => {
-  return (
-    <header className="fixed w-full bg-white shadow-md">
-      <div className="container mx-auto flex justify-between items-center py-6 px-4 md:px-8">
-        <Link href="/" className="text-3xl md:text-5xl font-bold text-gray-800 tracking-tight">
-          Splash App
-        </Link>
+  const pathname = usePathname();
+  const { count } = useCount();
 
-        {/* Button Group */}
+  return (
+    <header>
+      <div>
+        <Link href="/">Splash App</Link>
         <div className="flex gap-4">
-          <Button
-            buttonLabel="Sign In"
-            buttonBackgroundColor="red"
-          />
-          <Button
-            buttonLabel="Sign Up"
-            buttonBackgroundColor="blue"
-          />
+          {!["/counter-app"].includes(pathname) ? (
+            <>
+              <Button buttonLabel="Sign In" buttonBackgroundColor="red" />
+              <Button buttonLabel="Sign Up" buttonBackgroundColor="blue" />
+            </>
+          ) : (
+            <p className=" font-semibold text-lg">Current count : {count}</p>
+          )}
         </div>
       </div>
     </header>
--- alx-project-0x05/pages/_app.tsx
+++ alx-project-0x05/pages/_app.tsx
@@ -1,12 +1,14 @@
+import Layout from "@/components/layouts/Layout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import Layout from "@/components/layouts/Layout";
+import { CountProvider } from "@/context/CountContext";
 
 export default function App({ Component, pageProps }: AppProps) {
-  
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <CountProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </CountProvider>
   )
 }
